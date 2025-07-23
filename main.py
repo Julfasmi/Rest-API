cff5a93dc55fe2c5ee5fd5b541c7617b50f3be53
Julfasmi <126218541+Julfasmi@users.noreply.github.com>
2025-07-23 11:14:55 +0700

    update
    for deploy

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI 
+import uvicorn
+import os
 
 app = FastAPI(title="Simple Rest API")
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
